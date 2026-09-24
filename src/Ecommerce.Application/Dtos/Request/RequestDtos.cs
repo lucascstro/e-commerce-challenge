d5688cf8cfd5c24784666bc9edc20920c1c21fc7
lucascstro <lukas.castro@live.com>
2026-09-24 14:37:48 -0300

    style: remoção de comentários

--- src/Ecommerce.Application/Dtos/Request/RequestDtos.cs
+++ src/Ecommerce.Application/Dtos/Request/RequestDtos.cs
@@ -2,12 +2,7 @@ using Ecommerce.Domain.Entities.Enum;
 
 namespace Ecommerce.Application.Dtos.Request
 {
-    //criação de reserva do produto.
-    //cancelamento manual de reserva do produto.
-    //obter todas as reservas de um cliente.
     public record CreateReservationRequest(Guid CustomerId, Guid ProductId);
-
     public record UpdateReservationRequest(Guid ReservationId, Status Status);
-
     public record GetAllReservationsByCustomerRequest(Guid CustomerId);
 }
