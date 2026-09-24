39b3ed183bb6fadfeab96ca06ef798dd31043952
lucascstro <lukas.castro@live.com>
2026-09-24 15:14:35 -0300

    fix: alterado ProductName para ProductId, remoção dos record da classe.

--- src/Ecommerce.Application/Dtos/Response/ResponseDtos.cs
+++ src/Ecommerce.Application/Dtos/Response/ResponseDtos.cs
@@ -1,22 +1,19 @@
 namespace Ecommerce.Application.Dtos.Response
 {
-    public class ResponseDtos
-    {
-        public record AllReservationsByCustomerResponse(
-            Guid ReservationId,
-            string ProductName,
-            DateTime CreatedAt
-        );
+    public record AllReservationsByCustomerResponse(
+        Guid ReservationId,
+        Guid ProductId,
+        DateTime CreatedAt
+    );
 
-        public record CreateReservationResponse(
-            Guid ReservationId,
-            string ProductName,
-            DateTime CreatedAt
-        );
+    public record CreateReservationResponse(
+        Guid ReservationId,
+        Guid ProductId,
+        DateTime CreatedAt
+    );
 
-        public record UpdateReservationResponse(
-            Guid ReservationId,
-            string Status
-        );
-    }
+    public record UpdateReservationResponse(
+        Guid ReservationId,
+        string Status
+    );
 }
