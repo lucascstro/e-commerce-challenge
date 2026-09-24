dd5bcd0f50946838859c7129e189e3a4eddb106c
lucascstro <lukas.castro@live.com>
2026-09-23 21:55:18 -0300

    refactor: criado setter privado para propriedades.

--- src/Ecommerce.Domain/Entities/Customer.cs
+++ src/Ecommerce.Domain/Entities/Customer.cs
@@ -10,21 +10,20 @@ namespace Ecommerce.Domain.Entities
         public Guid CustomerId { get; private set; } = Guid.NewGuid();
         public string Name { get; private set; }
 
-        public Customer(string name)
-        {
-            Name = name;
-        }
+        public Customer(string name) => SetName(name);
         
-        public void UpdateName(string name)
+        public void UpdateName(string name) => SetName(name);
+
+        private void SetName(string name)
         {
+            if(string.IsNullOrWhiteSpace(name))
+                throw new DomainException("O nome não pode ser vazio ou espaços em branco.");
+
             if(name.Length > MaxNameLength)
                 throw new DomainException($"O nome não pode ter mais de {MaxNameLength} caracteres.");
                 
             if(name.Length < MinNameLength)
                 throw new DomainException($"O nome não pode ter menos de {MinNameLength} caracteres.");
-
-            if(string.IsNullOrWhiteSpace(name))
-                throw new DomainException("O nome não pode ser vazio ou espaços em branco.");
                         
             Name = name;
         }
--- src/Ecommerce.Domain/Entities/Product.cs
+++ src/Ecommerce.Domain/Entities/Product.cs
@@ -12,37 +12,16 @@ namespace Ecommerce.Domain.Entities
         public string? Description { get; private set; }
         public bool IsAvailable { get; private set; }
 
-        public Product(Guid productId, string name, string description, bool isAvailable)
+        public Product(string name, string description, bool isAvailable)
         {
-            ProductId = productId;
-            Name = name;
-            Description = description;
+            SetName(name);
+            SetDescription(description);
             IsAvailable = isAvailable;
         }
 
-        public void UpdateName(string name)
-        {
-            if(name.Length > MaxNameLength)
-                throw new DomainException($"O nome não pode ter mais de {MaxNameLength} caracteres.");
-
-            if(name.Length < MinNameLength)
-                throw new DomainException($"O nome não pode ter menos de {MinNameLength} caracteres.");
-
-            if(string.IsNullOrWhiteSpace(name))
-                throw new DomainException("O nome não pode ser vazio ou espaço em branco.");
-                        
-            Name = name;
-        }
-        public void UpdateDescription(string description)
-        {   
-            if(string.Compare(description, Description) == 0)
-                throw new DomainException("A descrição não pode ser igual a anterior.");
-
-            if(description.Length > MaxDescriptionLength)
-                throw new DomainException($"A descrição não pode ter mais de {MaxDescriptionLength} caracteres.");
-                        
-            Description = description;
-        }
+        public void UpdateName(string name) => SetName(name);
+        
+        public void UpdateDescription(string description) => SetDescription(description);
 
         public bool MarkAsUnavailable()
         {
@@ -60,6 +39,31 @@ namespace Ecommerce.Domain.Entities
 
             IsAvailable = true;
             return IsAvailable;
+        }
+
+        private void SetName(string name)
+        {
+            if(name.Length > MaxNameLength)
+                throw new DomainException($"O nome não pode ter mais de {MaxNameLength} caracteres.");
+
+            if(name.Length < MinNameLength)
+                throw new DomainException($"O nome não pode ter menos de {MinNameLength} caracteres.");
+
+            if(string.IsNullOrWhiteSpace(name))
+                throw new DomainException("O nome não pode ser vazio ou espaço em branco.");
+                        
+            Name = name;
+        }
+
+        private void SetDescription(string description)
+        {
+            if(string.Compare(description, Description) == 0)
+                throw new DomainException("A descrição não pode ser igual a anterior.");
+
+            if(description.Length > MaxDescriptionLength)
+                throw new DomainException($"A descrição não pode ter mais de {MaxDescriptionLength} caracteres.");
+                        
+            Description = description;
         }
     }
 }
