52545c5adde88bda8edcf0e7ba1038d58f65ae7b
lucascstro <lukas.castro@live.com>
2026-09-24 23:32:56 -0300

    feat: Alterado parametros do endpoint de reservar produto, criado endpoint de cancelar reserva e adicionado injeção de depedencia do ProductService

--- src/Ecommerce.Api/Controllers/ProductsController.cs
+++ src/Ecommerce.Api/Controllers/ProductsController.cs
@@ -1,3 +1,5 @@
+using Ecommerce.Application.Dtos.Request;
+using Ecommerce.Application.Services.Product;
 using Ecommerce.Application.Services.Reservation;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,20 +11,35 @@ namespace Ecommerce.Api.Controllers
     {
         private readonly ILogger<ProductsController> _logger;
         private readonly IReservationService _reservationService;
+        private readonly IProductService _productService;
 
-        public ProductsController(ILogger<ProductsController> logger, IReservationService reservationService)
+        public ProductsController(ILogger<ProductsController> logger, IReservationService reservationService, IProductService productService)
         {
             _logger = logger;
             _reservationService = reservationService;
+            _productService = productService;
         }
 
-        [HttpPost("{product_id}/{customer_id}/reserve")]
-        public async Task<IActionResult> ReserveProduct(Guid id_product, Guid id_customer, CancellationToken ct)
+        [HttpGet]
+        public async Task<IActionResult>GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync(CancellationToken.None);
+            return Ok(products);
+        }
+
+        [HttpPost("{id}/reserve")]
+        public async Task<IActionResult> ReserveProduct(Guid id, [FromQuery] Guid customer_id, CancellationToken ct)
         {
             //TODO verificar se utilizar o controle de usuários é um problema (pois no requisito menciona somente o ID DO PRODUTO na requisição). 
             //Gero o id do cliente aleatoriamente e a ideia era pegar o id do cliente logado ao fazer reserva.
-            
-            var reservationResponse = await _reservationService.CreateReservationAsync(id_customer, id_product, ct);
+            var reservationResponse = await _reservationService.CreateReservationAsync(new CreateReservationRequest(customer_id, id), ct);
+            return Ok(reservationResponse);
+        }
+
+        [HttpDelete("{reservation_id}/reserve")]
+        public async Task<IActionResult> CancelReservation(Guid reservation_id, CancellationToken ct)
+        {
+            var reservationResponse = await _reservationService.UpdateReservationToStatusCancelledAsync(reservation_id, ct);
             return Ok(reservationResponse);
         }
     }
