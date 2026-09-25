db8685f61f49196b4b3640bb09704cb42b1b0497
lucascstro <lukas.castro@live.com>
2026-09-25 00:07:40 -0300

    fix: alterando disponibilidade do produto conforme reserva

--- src/Ecommerce.Application/Services/Reservation/ReservationService.cs
+++ src/Ecommerce.Application/Services/Reservation/ReservationService.cs
@@ -8,10 +8,12 @@ namespace Ecommerce.Application.Services.Reservation
     public class ReservationService : IReservationService
     {
         private readonly IReservationRepository _reservationRepository;
+        private readonly IProductRepository _productRepository;
 
-        public ReservationService(IReservationRepository reservationRepository)
+        public ReservationService(IReservationRepository reservationRepository, IProductRepository productRepository)
         {
             _reservationRepository = reservationRepository;
+            _productRepository = productRepository;
         }
 
         public async Task<IEnumerable<AllReservationsByCustomerResponse>> GetAllReservationsByCustomerAsync(Guid customerId, CancellationToken ct)
@@ -37,7 +39,11 @@ namespace Ecommerce.Application.Services.Reservation
                 throw new ArgumentException("O ID do produto não pode ser vazio", nameof(request.ProductId));
 
             var reservation = new Domain.Entities.Reservation(request.CustomerId, request.ProductId);
+            var product = await _productRepository.GetByIdAsync(request.ProductId);
+            product.MarkAsUnavailable();
+            
             await _reservationRepository.AddAsync(reservation, ct);
+            await _productRepository.UpdateAsync(product);
             
             return new CreateReservationResponse(
                 reservation.ReservationId,
@@ -58,12 +64,16 @@ namespace Ecommerce.Application.Services.Reservation
                 throw new ArgumentException("O ID da reserva não pode ser vazio", nameof(reservationId));
 
             var reservation = await _reservationRepository.GetByIdAsync(reservationId, ct);
+            var product = await _productRepository.GetByIdAsync(reservation.ProductId);
 
             if(reservation == null)
                 throw new ArgumentException("Reserva não encontrada", nameof(reservationId));
 
             reservation.UpdateStatus(status);
+            product.MarkAsAvailable();
+            await _productRepository.UpdateAsync(product);
             await _reservationRepository.UpdateStatusAsync(reservation, ct);
+
             return new UpdateReservationResponse(
                 reservation.ReservationId,
                 reservation.Status
