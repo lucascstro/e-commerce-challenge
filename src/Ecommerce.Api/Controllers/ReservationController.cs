da9071030efcf3a502c322b2b93df87358d2bede
lucascstro <lukas.castro@live.com>
2026-09-24 21:16:04 -0300

    feat: criado controller de clientes com endpoit para recuperar reservas realizadas por cliente

---
+++ src/Ecommerce.Api/Controllers/ReservationController.cs
@@ -0,0 +1,26 @@
+using Ecommerce.Application.Services.Reservation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IReservationService _reservationService;
+
+        public CustomerController(ILogger<CustomerController> logger, IReservationService reservationService)
+        {
+            _logger = logger;
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("{id_customer}/reservations")]
+        public async Task<IActionResult> GetAllReservations(Guid id_customer, CancellationToken ct)
+        {
+            var reservations = await _reservationService.GetAllReservationsByCustomerAsync(id_customer, ct);
+            return Ok(reservations);
+        }
+    }
+}
