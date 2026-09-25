7c110bbac91f0ae62a5270227ca099eca34fd9fa
lucascstro <lukas.castro@live.com>
2026-09-24 21:52:08 -0300

    refactor: removido records não usados.

--- src/Ecommerce.Application/Dtos/Request/RequestDtos.cs
+++ src/Ecommerce.Application/Dtos/Request/RequestDtos.cs
@@ -3,6 +3,4 @@ using Ecommerce.Domain.Entities.Enum;
 namespace Ecommerce.Application.Dtos.Request
 {
     public record CreateReservationRequest(Guid CustomerId, Guid ProductId);
-    public record UpdateReservationRequest(Guid ReservationId, Status Status);
-    public record GetAllReservationsByCustomerRequest(Guid CustomerId);
 }
